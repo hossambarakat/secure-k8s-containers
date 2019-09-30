41cb46825ce245438fdd070fe11c37b8d55d4cd8
Hossam Barakat <hossam@Hossams-MacBook-Pro.local>
2019-10-01 09:25:06 +1000

    update clear-cluster script

--- clear-cluster.sh
+++ clear-cluster.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env sh
 
-kubectl delete pods --all
-kubectl delete deployment --all
-kubectl delete svc --all
-kubectl delete networkpolicies --all
-kubectl delete psp --all
+kubectl delete -f api-server --ignore-not-found=true
+kubectl delete -f network-policy --ignore-not-found=true
+kubectl delete -f psp --ignore-not-found=true
+kubectl delete -f security-context --ignore-not-found=true
 kubectl apply -f ./psp/privileged-psp.yaml
 clear
